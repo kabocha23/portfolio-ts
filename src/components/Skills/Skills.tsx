b92ac4f6fcaa2c4f25a05fd8717e0effaf4bef83
Jason Kobuchi <jekobuchi@gmail.com>
2023-01-20 10:59:23 -0800

    adding Skills component modified for TS

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import "./Skills.css";
+
+const Skills: FC = () => {
+  return (
+    <div className="skills-container">
+      <ul>
+        <li className="skills-item">
+          <p>React</p>
+        </li>
+        <li className="skills-item">
+          <p>Node.js</p>
+        </li>
+        <li className="skills-item">
+          <p>JavaScript (ES6+)</p>
+        </li>
+        <li className="skills-item">
+          <p>TypeScript</p>
+        </li>
+        <li className="skills-item">
+          <p>MySQL</p>
+        </li>
+        <li className="skills-item">
+          <p>MongoDB</p>
+        </li>
+        <li className="skills-item">
+          <p>Tableau</p>
+        </li>
+        <li className="skills-item">
+          <p>Google Data Studio</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
