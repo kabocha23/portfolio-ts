fb4878a6def1a3b13424e8126974a2d3ba3bbe60
Jason Kobuchi <jekobuchi@gmail.com>
2023-01-20 11:00:23 -0800

    adding FeaturedProject component modified for TS

---
+++ src/components/FeaturedProject/FeaturedProject.tsx
@@ -0,0 +1,75 @@
+import React, { forwardRef } from "react";
+import "./FeaturedProject.css";
+
+interface FeaturedProjectsProps {
+  pdId: number;
+  title: string;
+  headline: string;
+  caption: string;
+  image: string;
+  description: string;
+  sourceCode: string;
+  liveDemo: string;
+}
+
+const FeaturedProject = forwardRef<HTMLDivElement, FeaturedProjectsProps>(
+  (
+    {
+      pdId,
+      title,
+      headline,
+      caption,
+      image,
+      description,
+      sourceCode,
+      liveDemo,
+    },
+    ref
+  ) => {
+    return (
+      <div className="project-ref-detect" ref={ref}>
+        <div
+          className={
+            pdId % 2 === 0
+              ? "project-container isNotVisibleLeft"
+              : "project-container isNotVisibleRight"
+          }
+          id={`id-${pdId}`}
+        >
+          <div className="project-details">
+            <div className="project-verbiage">
+              <h1>{title}</h1>
+              <h3>{headline}</h3>
+              <h4>{caption}</h4>
+              <div className="project-description">
+                {description.split("\n").map((str) => (
+                  <p key={`${str}`}>{str}</p>
+                ))}
+              </div>
+
+              <button>
+                <a href={`${sourceCode}`} target="blank">
+                  Git Repo
+                </a>
+              </button>
+              {liveDemo ? (
+                <button>
+                  <a href={`${liveDemo}`} target="blank">
+                    Live Demo
+                  </a>
+                </button>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+          <div className="project-preview">
+            <img src={require(`../../Static/img/${image}.png`)}></img>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default FeaturedProject;
